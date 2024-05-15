7232a68a93fc382046510fcf7b1358ef9c6f945d
Md. Alamgir <agir97@gmail.com>
2024-05-15 20:46:33 +0600

    second commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -10,20 +10,13 @@ import { initializeApp } from "firebase/app";
 // console.log(import.meta.env.VITE_APIKEY);
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  // apiKey: "AIzaSyCF-dXPdvn7lJpTLIzor50Af7jkN7htmQE",
-  // authDomain: "accounting-client-31657.firebaseapp.com",
-  // projectId: "accounting-client-31657",
-  // storageBucket: "accounting-client-31657.appspot.com",
-  // messagingSenderId: "268874062426",
-  // appId: "1:268874062426:web:99ec6cf20f7b7868885663"
-
+const firebaseConfig = { 
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
