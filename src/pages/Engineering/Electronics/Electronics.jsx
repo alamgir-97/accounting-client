8eb0e5277347a559f489484576389fc06ce232d1
Md. Alamgir <agir97@gmail.com>
2026-06-30 21:48:47 +0600

    electronics page added in the Engineering section Improved

--- src/pages/Engineering/Electronics/Electronics.jsx
+++ src/pages/Engineering/Electronics/Electronics.jsx
@@ -5,12 +5,22 @@ const Electronics = () => {
     return (
         <div>
 <div>
+<img className="mx-auto" src="https://i.ibb.co/0j6Z3kM/electronics.jpg" alt="diode" border="0"></img>
     <h1 className="text-green-600 font-bold text-center">Battery</h1>
+    <p className="font-bold">Schematic Symbol:</p>
     <p className="font-bold">Functions:</p>
+    <p className="font-bold">Application:</p>
+    <p className="font-bold">Life Span/Lasting Time:</p>
     <p className="font-bold">Types:</p>
-    <p className="font-bold">Price:</p>
-    <p className="font-bold">Supplier:</p>
-    <p className="font-bold">Lasting Period:</p>
+    <p className="font-bold">Unit Price:</p>
+    <p className="font-bold">Pack/Lot Size:</p>
+    <p className="font-bold">Manufacturers:</p>
+    <p className="font-bold">Manufacture Time Required:</p>
+    <p className="font-bold">Materials used:</p>
+    <p className="font-bold">Shelf Life:</p>
+    <p className="font-bold">Storage Conditions:</p>
+    <p className="font-bold">Required Devices for Testing:</p>
+    <p className="font-bold">Required Devices for performance Measurement:</p>
 </div>
 <div className="m-4 p-8 flex flex-wrap md:flex-row gap-4">
 <button className="h-max-[35vw] border rounded-lg bg-orange-300 p-4 md:p-6 m-4 text-base md:text-lg lg:text-2xl text-left text-green-600 font-bold">Battery</button>
@@ -38,6 +48,7 @@ const Electronics = () => {
           <button className="h-max-[35vw] border rounded-lg bg-orange-300 p-4 md:p-6 m-4 text-base md:text-lg lg:text-2xl text-left"><a className='text-blue-800' href="https://www.youtube.com/@Engineering-Technology" target="_blank" rel="noreferrer">Engineering Technology | Electronics Website</a></button>
           <button className="h-max-[35vw] border rounded-lg bg-orange-300 p-4 md:p-6 m-4 text-base md:text-lg lg:text-2xl text-left"><a className='text-blue-800' href="https://www.youtube.com/@MasterElectronics62" target="_blank" rel="noreferrer">Engineering Technology | Electronics Website</a></button>
           <button className="h-max-[35vw] border rounded-lg bg-orange-300 p-4 md:p-6 m-4 text-base md:text-lg lg:text-2xl text-left"><a className='text-blue-800' href="https://www.youtube.com/watch?v=FtClUenzCRQ" target="_blank" rel="noreferrer">Top 10 Electronics Projects with Basic Components</a></button>
+          <button className="h-max-[35vw] border rounded-lg bg-orange-300 p-4 md:p-6 m-4 text-base md:text-lg lg:text-2xl text-left"><a className='text-blue-800' href="https://www.youtube.com/playlist?list=PLuUdFsbOK_8o1BzPcXHwILC7UN0MmTo5-" target="_blank" rel="noreferrer">Electronics & Electrical | by Sabin Civil Engineering</a></button>
           </div>
           
           
